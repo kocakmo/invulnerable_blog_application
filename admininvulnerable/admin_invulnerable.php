181ac358aaeb47211577ae171f4f54a6fa978c0d
kocakmo <46299266+kocakmo@users.noreply.github.com>
2019-01-02 14:37:22 +0300

    Update admin_invulnerable.php

--- admininvulnerable/admin_invulnerable.php
+++ admininvulnerable/admin_invulnerable.php
@@ -4,51 +4,34 @@
 <p><label></label><input type="submit" name="submit" value="Login"  /></p>
 </form>
 <?php
-
-if(isset($_POST['submit'])){
-
-    $username = mysql_real_escape_string($_POST['username']);
-    $password = mysql_real_escape_string($_POST['password']);
-    
+if(isset($_POST['submit']))
+{
 $servername = "localhost";
 $db_username = "root";
 $db_password = "usbw";
 $dbname = "admintable";
 	// Create connection
-$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+$conn = mysqli_connect($servername, $db_username, $db_password,$dbname );
+$username=mysql_real_escape_string($_POST["username"]);
+$password=mysql_real_escape_string($_POST["password"]);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 
 }
 
-$result=mysqli_query($conn,"SELECT password FROM users where username='$username' and password='$password'");
-$row = mysqli_fetch_row($result);
-$mat=$row[0];
-    
-	$hash = crypt('$mat');
-	echo $hash;
-	echo "<br>";
-	$hash2 = crypt($password, $hash);
-	echo $hash2;
-	
-	
-	
-    
-    if($result)
-	{
-	
-	
+$result=mysqli_query($conn,"SELECT * FROM users where username='".$username."' and password='".$password."'");
+    $row = $result->fetch_assoc();
 
-	
-    if($username=='admin' && (crypt($password, $hash) == $hash2)){ 
-		
-        //logged in return to index page
-        header('Location: adminindex_invulnerable.php');
-        exit;
+
+if($username=='admin' && $password==$row['password'])
+{	
+header('Location: adminindex_invulnerable.php');
+}
+
+else {
+         echo '<p class="error">Wrong username or password</p>';
     }
 	
 }
-	}
-
 ?>
